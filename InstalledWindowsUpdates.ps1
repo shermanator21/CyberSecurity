e8577d7c3bb877d1d5bef1b6a46c7639fa239245
Joseph Sherman <77992697+shermanator21@users.noreply.github.com>
2023-02-07 17:55:16 -0600

    Create InstalledWindowsUpdates.ps1

---
+++ InstalledWindowsUpdates.ps1
@@ -0,0 +1,14 @@
+# Get a list of all the installed Windows updates
+Get-WmiObject -Class Win32_QuickFixEngineering | Select-Object HotFixID, Description, InstalledOn
+
+<#
+This script uses the Get-WmiObject cmdlet to retrieve a list of all the installed Windows updates on the local computer. 
+The information retrieved includes the HotFixID (the KB number), Description, and InstalledOn date.
+
+Having a list of installed updates can be useful for a cybersecurity analyst in a number of ways, including:
+
+-Verifying that critical security updates have been installed
+-Investigating the root cause of a security issue by determining which updates were installed before or af
+ter the issue occurred
+-Keeping track of when updates were installed to ensure compliance with security policies or regulations
+#>
