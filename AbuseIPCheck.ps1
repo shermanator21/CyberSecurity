6df27743de73cd1fc405a00554d2a059dc784719
Joseph Sherman <77992697+shermanator21@users.noreply.github.com>
2023-02-08 13:52:03 -0600

    URL Concat
    Fixed error with URL concat

--- AbuseIPCheck.ps1
+++ AbuseIPCheck.ps1
@@ -7,7 +7,7 @@ $ips = Get-Content -Path "AbuseIPs.txt"
 # Loop through the list of IPs and retrieve information from abuseipdb.com
 foreach ($ip in $ips) {
   # Send the API request
-  $response = Invoke-WebRequest -Uri "$url?ip=$ip" -Method Get -Headers @{ "Key" = "4c0b559d3b4b87f66b80f8b8b1ee582532880a988ad0833cb2c6b6c552d75704deea210fd4322350" }
+  $response = Invoke-WebRequest -Uri "$url+$ip" -Method Get -Headers @{ "Key" = "4c0b559d3b4b87f66b80f8b8b1ee582532880a988ad0833cb2c6b6c552d75704deea210fd4322350" }
 
   # Parse the JSON response
   $result = $response.Content | ConvertFrom-Json
