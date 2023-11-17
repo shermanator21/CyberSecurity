0e0b0475eda5e5195468622a6663521267c259a4
Joseph Sherman <77992697+shermanator21@users.noreply.github.com>
2023-11-17 14:30:21 -0600

    Upload first iteration
    needed to query an AD group with over 5,000 members, and Get-ADGroupMember had a limit restraint of 5,000

---
+++ ADGroupMembers.ps1
@@ -0,0 +1,19 @@
+﻿# Input Group Name and outputs Username (SamAccountName) of members
+# This short script was created to query groups with large membership due to the limit restraints of Get-ADGroupMembers (5,000)
+
+# Specify Group Here
+$group = "InetFilter - Deploy Est" 
+
+# Gets DN of group
+$GroupDN = (Get-ADGroup -Identity $group).DistinguishedName
+
+# Gets DN of memebrs of group
+$members = DSget group $GroupDN -members
+
+# Strips Quotation marks to help parse below
+$members = $members.Replace("`"","")
+
+# Cycle through each member and output SamAccountName and DN
+Foreach ($member in $members) {
+Get-AdUser -Filter {DistinguishedName -eq $member} | select SAMAccountName
+}
